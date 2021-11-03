fc118187bf07aa35c0ab9f9cfcce59eb058f34de
adiy55 <adiyafe55@gmail.com>
2021-11-03 21:46:24 +0200

    Edit main

--- main.c
+++ main.c
@@ -37,27 +37,6 @@ int main() { // 0 = false, 1 = true
         }
     }
     printf("\n");
-//    int num, ans;
-//
-//    printf("isStrong- Please enter a number: ");
-//    scanf("%d", &num);
-//    ans = isStrong(num);
-//    printf("%d\n", ans);
-//
-//    printf("isPrime- Please enter a number: ");
-//    scanf("%d", &num);
-//    ans = isPrime(num);
-//    printf("%d\n", ans);
-//
-//    printf("isArmstrong- Please enter a number: ");
-//    scanf("%d", &num);
-//    ans = isArmstrong(num);
-//    printf("%d\n", ans);
-//
-//    printf("isPalindrome- Please enter a number: ");
-//    scanf("%d", &num);
-//    ans = isPalindrome(num);
-//    printf("%d\n", ans);
 
     return 0;
 }
